3d107c9368b1d4feb585427f76542aa2b69794ba
burmasovanton <looseangel1@gmail.com>
2023-01-09 07:41:54 +0500

    initial

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 from config import TOKEN
+import random
 
 
 bot = Bot(token=TOKEN)
@@ -11,17 +12,20 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+    await message.reply("Привет!\nЭто бот, который радует пользователей анекдотами. Напиши \help, чтобы получить справку.")
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+    await message.reply("Наберите \start - для начала работы. Наберите \help для справки. Наберите \joke для получения анекдота! ")
 
+f = open('anecdotes.txt', 'r', encoding='UTF-8') as jokes:
+lines = jokes.readlines()
+random_line = random.choice(lines).strip()
+@dp.message_handler(commands=['joke'])
+async def process_start_command(message: types.Message):
+    await message.reply(random_line)
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
 
 
 if __name__ == '__main__':
