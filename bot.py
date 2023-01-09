1dd0702689aa3799540e995abf6af526fd5c5776
burmasovanton <looseangel1@gmail.com>
2023-01-09 09:00:41 +0500

    initial

--- bot.py
+++ bot.py
@@ -5,29 +5,33 @@ from aiogram.utils import executor
 from config import TOKEN
 import random
 
-
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nЭто бот, который радует пользователей анекдотами. Напиши /help, чтобы получить справку.")
-
-
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("Наберите /start - для начала работы. Наберите /help для справки. Наберите /joke для получения анекдота! ")
 def random_joke():
     with open('anecdotes.txt', 'r', encoding='UTF-8') as jokes:
         lines = jokes.readlines()
         random_line = random.choice(lines).strip()
         return random_line
+
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nЭто бот, который радует пользователей анекдотами. Напиши /help, чтобы получить "
+                        "справку.")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply(
+        "Наберите /start - для начала работы. Наберите /help для справки. Наберите /joke для получения анекдота! ")
+
+
+
 @dp.message_handler(commands=['joke'])
 async def process_help_command(message: types.Message):
     await message.reply(random_joke())
-
-
 
 
 if __name__ == '__main__':
